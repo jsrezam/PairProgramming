b9098522c93f082eaf9ad143d1b5f74b6648bca7
Sebastian Reza <jsrezam@gmail.com>
2021-11-19 18:58:29 -0500

    Second Commit (Jeanneth Molina)

--- Program.cs
+++ Program.cs
@@ -17,7 +17,21 @@ namespace PairProgramming
             {
                 Console.WriteLine($"[1] Push (1) to add {increment} point to player 1\n[2] Push (2) to add {increment} point to player 2\n");
                 string opt = Console.ReadLine();
-                Console.Clear();                
+                Console.Clear();
+
+                if (opt.Equals("1"))
+                    player1 += increment;
+                else if (opt.Equals("2"))
+                    player2 += increment;
+                else
+                    Console.WriteLine("Error:> Invalid option try again");
+
+                
+                if (player1 >= winScore || player2 >= winScore)
+                {
+                    Console.Clear();
+                    break;
+                }
             }
 
             Console.ReadLine();
