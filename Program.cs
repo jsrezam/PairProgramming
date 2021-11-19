66f7e30b83d626a4b6631e880da9cfa857327d14
Sebastian Reza <jsrezam@gmail.com>
2021-11-19 18:57:22 -0500

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace PairProgramming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            int player1 = 0;
+            int player2 = 0;
+            int increment = 4;
+            int winScore = 42;
+
+            Console.WriteLine("Match started !!\n");
+
+            while (true)
+            {
+                Console.WriteLine($"[1] Push (1) to add {increment} point to player 1\n[2] Push (2) to add {increment} point to player 2\n");
+                string opt = Console.ReadLine();
+                Console.Clear();                
+            }
+
+            Console.ReadLine();
+        }        
     }
 }
